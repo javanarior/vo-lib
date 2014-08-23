b4421db43b40f886ac1802fa6f8a45c19eed12c0
Sven <javanarior@gmail.com>
2014-08-23 20:27:25 +0200

    - add tests

--- src/test/java/de/javanarior/vo/types/AbstractValueTest.java
+++ src/test/java/de/javanarior/vo/types/AbstractValueTest.java
@@ -17,6 +17,7 @@ package de.javanarior.vo.types;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -134,6 +135,22 @@ public class AbstractValueTest {
         assertFalse(testee.isNull());
     }
 
+    public void testEquals() {
+        AbstractValueImpl otherValue = new AbstractValueImpl(AbstractValueImpl.class, "12");
+        assertTrue(testee.equals(otherValue));
+        assertTrue(otherValue.equals(testee));
+    }
+
+    public void testEqualsSameObject() {
+        assertTrue(testee.equals(testee));
+    }
+
+    public void testEqualsForDifferentTypes() {
+        OtherAbstractValueImpl otherValue = new OtherAbstractValueImpl(OtherAbstractValueImpl.class, "12");
+        assertFalse(testee.equals(otherValue));
+        assertFalse(otherValue.equals(testee));
+    }
+
 
 
     private static class AbstractValueImpl extends AbstractValue<AbstractValueImpl> {
@@ -151,6 +168,25 @@ public class AbstractValueTest {
 
         @Override
         public int compareTo(AbstractValueImpl other) {
+            return 0;
+        }
+    }
+
+    private static class OtherAbstractValueImpl extends AbstractValue<OtherAbstractValueImpl> {
+
+        private Object value;
+        protected OtherAbstractValueImpl(Class<OtherAbstractValueImpl> metaType, Object value) {
+            super(metaType);
+            this.value = value;
+        }
+
+        @Override
+        public Object getValue() {
+            return value;
+        }
+
+        @Override
+        public int compareTo(OtherAbstractValueImpl other) {
             return 0;
         }
     }
